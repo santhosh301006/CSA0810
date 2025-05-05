010c3d7a7cec1c0b142b0b92a2cc4167fea71663
santhosh301006 <sunnapusanthosh117@gmail.com>
2025-05-05 17:56:17 +0530

    Add files via upload

---
+++ 6 SUM OF SQUARES OF THE 1ST N NATURAL NUMBERS.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a number: "))
+sum = 0
+for i in range(1, n+1):
+    sum += i**2
+print("Sum of squares:", sum)
---
+++ 6 reverse a string.py
@@ -0,0 +1,5 @@
+string = input("Enter a string: ")
+reversed_str = ''
+for char in string:
+    reversed_str = char + reversed_str
+print("Reversed string:", reversed_str)
